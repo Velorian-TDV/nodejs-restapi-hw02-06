77b4fd35c1afe39765f6a365f59d0ee6104e0978
Velorian <TerleckyDima92@gmail.com>
2023-10-27 20:44:32 +0300

    Homework 05 done

--- routes/api/auth-router.js
+++ routes/api/auth-router.js
@@ -2,6 +2,7 @@ import express from 'express';
 import authController from '../../controllers/auth-controller.js';
 import isEmptyBody from '../../middlewares/isEmptyBody.js';
 import authenticate from '../../middlewares/authenticate.js';
+import upload from '../../middlewares/upload.js';
 import validateBody from '../../helpers/validateBody.js';
 import { userAuthorizationJoiSchema, userRegistrationJoiSchema } from '../../models/user-model.js';
 
@@ -12,6 +13,7 @@ const userAuthorizationValidate = validateBody(userAuthorizationJoiSchema);
 authRouter.post('/register', isEmptyBody, userRegistrationValidate, authController.registration);
 authRouter.post('/login', isEmptyBody, userAuthorizationValidate, authController.authorization);
 authRouter.get('/current', authenticate, authController.getCurrent);
-authRouter.post('/logout', authenticate, authController.logOut)
+authRouter.post('/logout', authenticate, authController.logOut);
+authRouter.patch('/avatars', authenticate, upload.single('avatar'), authController.updateAvatar);
 
 export default authRouter;
